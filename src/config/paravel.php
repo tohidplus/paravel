cd4ee31d7ac9b241bc3bad8f6c139875a2ac96d1
Tohid Dadashnejad <tohid@dataak.com>
2020-09-06 15:59:02 +0430

    Finish up new documentation

--- src/config/paravel.php
+++ src/config/paravel.php
@@ -1,6 +1,4 @@
 <?php
 return [
     'artisan_path' => env('PARAVEL_ARTISAN_PATH', '/var/www/artisan'),
-    'redis_connection' => env('PARAVEL_REDIS_CONNECTION', 'default'),
-    'waiting_timeout' => env('PARAVEL_WAITING_TIMEOUT', 30)
 ];
